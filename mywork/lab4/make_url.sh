5096e9ecab70fe4dba0fc512a8ad3f675045127a
averydonmoyer <hdj3fw@virginia.edu>
2024-03-15 17:05:57 -0400

    fixed the bash script

--- mywork/lab4/make_url.sh
+++ mywork/lab4/make_url.sh
@@ -1,9 +1,9 @@
 #!/bin/bash 
 
-curl media.distractify.com/brand-img/uq6IB4wkF/0x0/trisha-paytas-family-1700624791023.jpg > trisha
+curl https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExOTBza2dtc3Vxemp2NWo3eWZ3cmNnNjl6MWNreXZyYmkyeTBqcWlsdyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/l378juKkfz46yumQw/giphy.gif > trisha
 
 aws s3 cp trisha s3://ds2002-hdj3fw
 
 aws s3 presign --expires-in 604800 s3://ds2002-hdj3fw/trisha
 
-echo https://ds2002-hdj3fw.s3.amazonaws.com/trisha?AWSAccessKeyId=AKIATCKARYADVJN6RKKY&Signature=B1U8J%2B4voXofaJb9O4CDa8%2BM%2Bw8%3D&Expires=1711133037
+echo https://ds2002-hdj3fw.s3.amazonaws.com/trisha?AWSAccessKeyId=AKIATCKARYADVJN6RKKY&Signature=zvjw83jnC9oESthWcnpSQlUlJxE%3D&Expires=1711141489 
